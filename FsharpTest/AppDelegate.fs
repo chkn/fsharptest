8b488ce1521b5039b482616b1aaefd4ea0936bcc
Alex Corrado <alexc@xamarin.com>
2014-06-17 19:43:54 -0400

    Initial commit

---
+++ FsharpTest/AppDelegate.fs
@@ -0,0 +1,26 @@
+﻿namespace FsharpTest
+
+open System
+open MonoTouch.UIKit
+open MonoTouch.Foundation
+
+[<Register ("AppDelegate")>]
+type AppDelegate () =
+    inherit UIApplicationDelegate ()
+
+    let mutable window = null
+    override this.Window
+        with get () = window
+        and set value = window <- value
+
+    // This method is invoked when the application is ready to run.
+    override this.FinishedLaunching (app, options) =
+        this.Window.MakeKeyAndVisible ()
+        true
+
+module Main =
+    [<EntryPoint>]
+    let main args =
+        UIApplication.Main (args, null, "AppDelegate")
+        0
+
